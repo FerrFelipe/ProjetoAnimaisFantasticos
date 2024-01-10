5b5ac18431dd66b383c1a8d91abc208626c52a48
Felipe <fefelipe21@gmail.com>
2024-01-10 19:57:28 -0300

    feat: add scrollSuave

--- script.js
+++ script.js
@@ -33,3 +33,21 @@ function initAccordion() {
   }
 }
 initAccordion();
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+  if (linksInternos) {
+    function scrollSuave(event) {
+      event.preventDefault();
+      const href = event.currentTarget.getAttribute("href");
+      const section = document.querySelector(href);
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    linksInternos.forEach((link) => {
+      link.addEventListener("click", scrollSuave);
+    });
+  }
+}
+initScrollSuave();
