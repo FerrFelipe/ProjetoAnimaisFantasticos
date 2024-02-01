2e342031ef617ddbc8ea93339a8addc642a53584
Felipe <fefelipe21@gmail.com>
2024-02-01 13:21:59 -0300

    update: new js

---
+++ js/modules/tab-nav.js
@@ -0,0 +1,20 @@
+export default function initTabNav() {
+  const tabmenu = document.querySelectorAll('[data-tab="menu"] li');
+  const tabcontent = document.querySelectorAll('[data-tab="content"] section');
+
+  if (tabmenu.length && tabcontent.length) {
+    tabcontent[0].classList.add("ativo", tabcontent[0].dataset.anime);
+    function activetab(index) {
+      tabcontent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      const direcao = tabcontent[index].dataset.anime;
+      tabcontent[index].classList.add("ativo", direcao);
+    }
+    tabmenu.forEach((itemmenu, index) => {
+      itemmenu.addEventListener("click", () => {
+        activetab(index);
+      });
+    });
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,8 @@
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimaScroll from "./modules/scroll-animacao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tab-nav.js";
+initScrollSuave();
+initAnimaScroll();
+initAccordion();
+initTabNav();
